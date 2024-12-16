39744b44c9a81e47a3cfccff7aafeb5b11acb45d
AhmedAbdullah125 <ahmednabeeh98@gmail.com>
2024-12-16 20:47:53 +0200

    eee

--- src/components/home/Recorder.jsx
+++ src/components/home/Recorder.jsx
@@ -86,8 +86,8 @@ const RecordView = (props) => {
       </div> */}
       <div style={isStoped && props.isRecord ? { display: "block" } : { display: "none" }} className="player">
         {" "}
-        {/* <video src={mediaBlobUrl} controls style={{ height: "54px" }} className="md:w-56 w-44" /> */}
-        <audio src={mediaBlobUrl} controls style={{ height: "54px" }} className="md:w-56 w-44" />
+        <video src={mediaBlobUrl}  controls style={{ height: "54px" }} className="md:w-56 w-44" />
+        {/* <audio src={mediaBlobUrl} controls style={{ height: "54px" }} className="md:w-56 w-44" /> */}
         {/* <AudioPlayer mediaBlobUrl={mediaBlobUrl} /> */}
       </div>
       {/* <AudioPlayer /> */}
